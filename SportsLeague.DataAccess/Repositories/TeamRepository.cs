ab8b55a4a45187aef1dde5042e0349632b8c18c9
JUAN MIGUEL ARANGO ARANGO <juanarango1113643@correo.itm.edu.co>
2026-03-31 12:15:55 -0500

    FASE 3TERMINADA

--- SportsLeague.DataAccess/Repositories/TeamRepository.cs
+++ SportsLeague.DataAccess/Repositories/TeamRepository.cs
@@ -1,23 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
 using SportsLeague.DataAccess.Context;
-
 using SportsLeague.Domain.Entities;
-
 using SportsLeague.Domain.Interfaces.Repositories;
-
-
 namespace SportsLeague.DataAccess.Repositories;
 
-
 public class TeamRepository : GenericRepository<Team>, ITeamRepository
-
 {
-
     public TeamRepository(LeagueDbContext context) : base(context)
     {
     }
-
     public async Task<Team?> GetByNameAsync(string name)//devuelve un objeto de tipo TEAM
     {
         return await _dbSet
